b719c0f22673179a236f19662f4be52b6f293bed
Jesse Houwing <jesse.houwing@gmail.com>
2022-02-02 00:34:32 +0100

    Initial commit

---
+++ ppt-diffmerge-tool/Program.cs
@@ -0,0 +1,177 @@
+﻿using ManyConsole;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using PowerPointApplication = Microsoft.Office.Interop.PowerPoint.Application;
+using Microsoft.Office.Interop.PowerPoint;
+
+namespace ppt_diffmerge_tool
+{
+    internal class MergeCommand : ConsoleCommand
+    {
+        readonly ManualResetEvent handle = new ManualResetEvent(false);
+
+        public string @Base
+        {
+            get;
+            set;
+        }
+
+        public string Local
+        {
+            get; set;
+        }
+
+        public string Remote
+        {
+            get; set;
+        }
+
+        public string Output
+        {
+            get; set;
+        }
+
+        public MergeCommand()
+        {
+            IsCommand("merge", "Merges 2 powerpoint files.");
+            HasRequiredOption<string>("l|local=", "Local file", f => Local = f);
+            HasRequiredOption<string>("r|remote=", "Remote file", f => Remote = f);
+            HasRequiredOption<string>("o|output=", "Output file", f => Output = f);
+            HasOption<string>("b|base=", "Base file", f => Base = f);
+        }
+
+        public override int Run(string[] remainingArguments)
+        {
+            PowerPointApplication app = null;
+            Presentation presentation = null;
+
+            try
+            {
+                app = new PowerPointApplication();
+                app.PresentationCloseFinal += App_PresentationClose;
+
+                File.Copy(Local, Output, true);
+                presentation = app.Presentations.Open(Output);
+
+                if (string.IsNullOrWhiteSpace(Base))
+                {
+                    presentation.Merge(Remote);
+                }
+                else
+                {
+                    presentation.MergeWithBaseline(Remote, Base);
+                }
+
+                handle.WaitOne();
+            }
+            finally
+            {
+                Marshal.ReleaseComObject(presentation);
+                Marshal.ReleaseComObject(app);
+            }
+            return 0;
+        }
+
+        private void App_PresentationClose(Presentation presentation)
+        {
+            handle.Set();
+        }
+    }
+
+    internal class DiffCommand : ConsoleCommand
+    {
+        readonly ManualResetEvent handle = new ManualResetEvent(false);
+
+        public string @Base
+        {
+            get;
+            set;
+        }
+
+        public string Local
+        {
+            get; set;
+        }
+
+        public string Remote
+        {
+            get; set;
+        }
+
+        public DiffCommand()
+        {
+            IsCommand("diff", "Diffs 2 powerpoint files.");
+            HasRequiredOption<string>("l|local=", "Local file", f => Local = f);
+            HasRequiredOption<string>("r|remote=", "Remote file", f => Remote = f);
+            HasOption<string>("b|base=", "Base file", f => Base = f);
+        }
+
+        public override int Run(string[] remainingArguments)
+        {
+            PowerPointApplication app = null;
+            Presentation presentation = null;
+
+            try
+            {
+                app = new PowerPointApplication();
+                app.PresentationCloseFinal += App_PresentationClose;
+
+                presentation = app.Presentations.Open(Local, Microsoft.Office.Core.MsoTriState.msoTrue);
+
+                if (string.IsNullOrWhiteSpace(Base))
+                {
+                    presentation.Merge(Remote);
+                }
+                else
+                {
+                    presentation.MergeWithBaseline(Remote, Base);
+                }
+
+                handle.WaitOne();
+            }
+            finally
+            {
+                Marshal.ReleaseComObject(presentation);
+                Marshal.ReleaseComObject(app);
+            }
+            return 0;
+        }
+
+        private void App_PresentationClose(Presentation presentation)
+        {
+            handle.Set();
+        }
+    }
+
+    internal static class Program
+    {
+        [STAThread]
+        static int Main(string[] args)
+        {
+            int result;
+            try
+            {
+                ConsoleCommand[] commands = { new DiffCommand(), new MergeCommand() };
+                result = ConsoleCommandDispatcher.DispatchCommand(commands, args, Console.Out);
+            }
+            catch (Exception e)
+            {
+                result = -1;
+                Console.Error.WriteLine(e.Message);
+                if (e.InnerException != null)
+                {
+                    Console.Error.WriteLine(e.InnerException.Message);
+                }
+            }
+
+            Environment.Exit(result);
+            return result;
+        }
+    }
+}
---
+++ ppt-diffmerge/Form1.cs
@@ -0,0 +1,10 @@
+namespace ppt_diffmerge
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
